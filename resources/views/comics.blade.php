041e179b7466590f477053c7f762b220e419b64c
paoloevan <paoloev1900@gmail.com>
2022-12-16 14:46:01 +0100

    add cards

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,5 +1,16 @@
 @extends('layout.app')
 
 @section('content')
-<h1>main</h1>
+<div class="container">
+    <div class="row row-cols-6">
+        @foreach ($comics as $comic)
+        <div class="col">
+            <div class="card_item">
+                <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                {{$comic['title']}}
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection

fa5ec374648220169fcde5446174a154b47bbbb6
paoloevan <paoloev1900@gmail.com>
2022-12-16 15:13:27 +0100

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,27 @@
-<h1>header</h1>
+<header>
+    <div class="header_top text-end text-uppercase">
+        <span>dc power visa</span>
+        <span>additional dc site</span>
+    </div>
+    <div class="container ">
+
+        <img src="{{ Vite::asset('resources/images/dc-logo.png')}}" alt="">
+
+        <div class="navbar">
+            <a href="">characters</a>
+            <a href="">comics</a>
+            <a href="">movies</a>
+            <a href="">tv</a>
+            <a href="">games</a>
+            <a href="">collectibles</a>
+            <a href="">videos</a>
+            <a href="">fans</a>
+            <a href="">news</a>
+            <a href="">shop</a>
+        </div>
+
+        <div class="search">
+            <input type="text" placeholder="search">
+        </div>
+    </div>
+</header>
